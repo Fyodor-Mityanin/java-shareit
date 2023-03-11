c9ce3b2da020f9321c51f5536dac35b36a2ed615
Митянин Фёдор Юрьевич <f.mityanin@e2e4online.ru>
2023-03-11 22:03:29 +0700

    + тесты

---
+++ server/src/test/java/ru/practicum/shareit/comment/CommentBookingRepositoryTest.java
@@ -0,0 +1,43 @@
+package ru.practicum.shareit.comment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import ru.practicum.shareit.comment.dto.CommentDto;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static ru.practicum.shareit.ObjectMaker.*;
+
+@DataJpaTest
+public class CommentBookingRepositoryTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private CommentRepository repository;
+
+    @Test
+    public void shouldFindByItemIdWithAuthor() {
+
+    }
+
+    @Test
+    public void shouldStoreBooking() {
+        User owner = entityManager.persist(makeUser(null, "Пётр", "ivanov@mail.ru"));
+        User commentator = entityManager.persist(makeUser(null, "Иван", "petrov@mail.ru"));
+        Item item = entityManager.persist(makeItem(null, "Итем", "Описание", owner, true));
+        entityManager.persist(makeComment("коментарий", item, commentator));
+
+        List<CommentDto> comments = repository.findByItemIdWithAuthor(item.getId());
+
+        assertThat(comments)
+                .hasSize(1)
+                .element(0)
+                .hasFieldOrPropertyWithValue("text", "коментарий");
+    }
+}
